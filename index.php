9b117e3e6c281b0c946970e9f083b189a5369ef6
Alessandro <marzucoalessandro1@gmail.com>
2021-02-15 17:46:29 +0100

    ok esercizio

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 // Create una classe per descrivere un entitá, assegnatele degli attributi, utilizzate il constructor. Create alcune istanze della classe. Stampate a schermo gli attributi di ogni istanza.
 // Per chi non ha fantasia ecco qualche entitá che potete usare:
 // Animale, Persona, Casa, Computer, Hotel, Film, Videogioco, Sport, Bicicletta, Veicolo, Nazione etc.
+
 class Hotel {
   public $name;
   public $city;
@@ -10,31 +11,39 @@ class Hotel {
   public $stars;
   public $roomsNumber;
   public $parking;
+  public $foto;
 
-  function __construct(string $name, string $city, string $address, int $stars, int $roomsNumber, string $parking){
+
+  function __construct(string $name, string $city, string $address, int $stars, int $roomsNumber, string $parking, string $foto){
     $this ->name = $name;
     $this ->city = $city;
     $this ->address = $address;
     $this ->stars = $stars;
     $this ->roomsNumber = $roomsNumber;
     $this ->parking = $parking;
+    $this ->foto = $foto;
+
   }
 
+  public function setSeaWatch($SeaWatch){
+    $this ->seaWatch = $SeaWatch;
+  }
 
 }
 
-$Ambassador = new Hotel("Ambassador", "Roma", "Via dei gigli 51", 3, 25, "si");
+$Ambassador = new Hotel("Ambassador", "Roma", "Via dei gigli 51", 3, 25, "si", "https://cf.bstatic.com/xdata/images/hotel/270x200/65251888.jpg?k=200afe82f7ae1716f74f53d6f45d899f7cc1718031ea5b83e2645063140a4ea5&o=");
 // var_dump($Ambassador);
-$Splendid = new Hotel("Splendid", "Milano", "Corso Mazzini 23", 4, 30, "si");
-$Vittoria = new Hotel("Vittoria", "Firenze", "Piazzale Flaminio 3", 5, 50, "no");
-$Astrid = new Hotel("Astrid", "Bologna", "Via Ottavia 12", 2, 81, "si");
-$Belvedere = new Hotel("Belvedere", "Napoli", "Piazza del Plebiscito 1", 3, 120, "no");
-$Majestic = new Hotel("Majestic", "Bari", "Via delle Carrozze 20", 4, 10, "no");
-$Miramare = new Hotel("Miramare", "Palermo", "Via Sicilia 1", 5, 30, "si");
+$Splendid = new Hotel("Splendid", "Milano", "Corso Mazzini 23", 4, 30, "si", "https://cf.bstatic.com/images/hotel/max1024x768/626/62647098.jpg");
+$Vittoria = new Hotel("Vittoria", "Firenze", "Piazzale Flaminio 3", 5, 50, "no", "https://2.bp.blogspot.com/-MwkxNgVn5h8/XIbHfdB_bgI/AAAAAAABOc0/fPYtUgMBBColW6uH2kvpKbkNm91sni72QCLcBGAs/s1600/hotel%2Bspa%2Bmadonna%2Bdi%2Bcampiglio.jpg");
+$Astrid = new Hotel("Astrid", "Bologna", "Via Ottavia 12", 2, 81, "si", "https://cf.bstatic.com/xdata/images/hotel/270x200/65251888.jpg?k=200afe82f7ae1716f74f53d6f45d899f7cc1718031ea5b83e2645063140a4ea5&o=");
+$Belvedere = new Hotel("Belvedere", "Napoli", "Piazza del Plebiscito 1", 3, 120, "no", "https://cf.bstatic.com/xdata/images/hotel/270x200/65251888.jpg?k=200afe82f7ae1716f74f53d6f45d899f7cc1718031ea5b83e2645063140a4ea5&o=");
+$Majestic = new Hotel("Majestic", "Bari", "Via delle Carrozze 20", 4, 10, "no", "https://www.suiteweb.it/datastore/uploads/837/900_0/1500204093ff57cf665a70e01faedf2351804d65ea.jpg");
+$Miramare = new Hotel("Miramare", "Palermo", "Via Sicilia 1", 5, 30, "si", "https://www.hotelcrozzon.com/img/TOP/index2.jpg");
 $Hotel = [$Ambassador, $Splendid, $Vittoria, $Astrid, $Belvedere, $Majestic, $Miramare];
 
+$Astrid ->setSeaWatch("no");
 
-// var_dump($Hotel);
+var_dump($Hotel);
 
  ?>
 
@@ -68,6 +77,9 @@ $Hotel = [$Ambassador, $Splendid, $Vittoria, $Astrid, $Belvedere, $Majestic, $Mi
         margin: 20px;
         width: 400px;
       }
+      img{
+        width: 100%;
+      }
 
       </style>
       <body>
@@ -79,6 +91,7 @@ $Hotel = [$Ambassador, $Splendid, $Vittoria, $Astrid, $Belvedere, $Majestic, $Mi
             <?php if ($value->parking == "si"): ?>
 
               <div class="hotel1">
+                <img src="<?php echo ($value->foto); ?>" alt="">
                 <h1>Nome: <?php echo ($value->name); ?></h1>
                 <h3>Città: <?php echo ($value->city); ?></h3>
                 <p>Indirizzo: <?php echo ($value->address); ?></p>
@@ -89,6 +102,7 @@ $Hotel = [$Ambassador, $Splendid, $Vittoria, $Astrid, $Belvedere, $Majestic, $Mi
             <?php else: ?>
 
               <div class="hotel2">
+                <img src="<?php echo ($value->foto); ?>" alt="">
                 <h1>Nome: <?php echo ($value->name); ?></h1>
                 <h3>Città: <?php echo ($value->city); ?></h3>
                 <p>Indirizzo: <?php echo ($value->address); ?></p>
